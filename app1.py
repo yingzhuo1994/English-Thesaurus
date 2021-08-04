a1c8f86e7e265b149275b9133477ea754c9cecb4
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-04 13:24:20 -0500

    Version 1.2, add acronym search function

--- app1.py
+++ app1.py
@@ -8,8 +8,12 @@ def translate(w):
     if w in data:
         return data[w]
     elif w.title() in data:
+        # proper noun search
         # title() method will convert the first letter to uppercase and the rest to lowercase. 
         return data[w.title()]
+    elif w.upper() in data:
+        # acronym search
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         close_match = get_close_matches(w, data.keys())[0]
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % close_match)
