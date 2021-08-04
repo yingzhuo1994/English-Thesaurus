ddf66876fa53699c20055f8e03853817db7b68b0
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-03 23:48:51 -0500

    Create the first py file

---
+++ app1.py
@@ -0,0 +1,14 @@
+import json
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+
+print(translate(word))
